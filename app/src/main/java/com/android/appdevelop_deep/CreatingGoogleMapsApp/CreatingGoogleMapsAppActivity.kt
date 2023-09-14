da898a86eab647dd21c433c1898bf027231b524f
seongssu <loveforggong@gmail.com>
2023-09-14 16:39:05 +0900

    [FEAT] : ⚙️ 앱에 지도표시(OnMapReadyCallback인터페이스 상속)

--- app/src/main/java/com/android/appdevelop_deep/CreatingGoogleMapsApp/CreatingGoogleMapsAppActivity.kt
+++ app/src/main/java/com/android/appdevelop_deep/CreatingGoogleMapsApp/CreatingGoogleMapsAppActivity.kt
@@ -2,14 +2,22 @@ package com.android.appdevelop_deep.CreatingGoogleMapsApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.appdevelop_deep.R
 import com.android.appdevelop_deep.databinding.ActivityCreatingGoogleMapAppBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class CreatingGoogleMapsAppActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityCreatingGoogleMapAppBinding.inflate(layoutInflater) }
+class CreatingGoogleMapsAppActivity : AppCompatActivity(), OnMapReadyCallback {
+    var googleMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_creating_google_map_app)
 
-
+        (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment)!!
+            .getMapAsync(this)
+    }
+    override fun onMapReady(p0: GoogleMap) {
+        googleMap = p0
     }
 }
