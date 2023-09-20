9d2750ebf37c52ca2af269a0fba8623459ab2fab
seongssu <loveforggong@gmail.com>
2023-09-20 16:49:08 +0900

    [FEAT] : ⚙️ SPF에 데이터 직렬화 후 저장, 역직렬화 후 어댑터에 데이터 넣어줌

--- app/src/main/java/com/android/imagesearch/SearchFragment/SearchFragment.kt
+++ app/src/main/java/com/android/imagesearch/SearchFragment/SearchFragment.kt
@@ -11,18 +11,25 @@ import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.android.imagesearch.Method.shortToast
 import com.android.imagesearch.api.Document
 import com.android.imagesearch.api.NetWorkClient
 import com.android.imagesearch.databinding.FragmentSearchBinding
+import com.android.imagesearch.sharedPreferences.SPF
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
     private val binding by lazy { FragmentSearchBinding.inflate(layoutInflater) }
+    private lateinit var spf : SPF
     var items = ArrayList<Document>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        spf = SPF(requireContext())
+
         binding.searchView.apply {
             isSubmitButtonEnabled = true
             setOnQueryTextListener(object : SearchView.OnQueryTextListener {
@@ -49,14 +56,22 @@ class SearchFragment : Fragment() {
         requireActivity().runOnUiThread {
             if (responseData.documents != null) {
                 items = responseData.documents
+                val gson = Gson()
+                val save_items = gson.toJson(items)
+                spf.saveData(save_items)
+
+                val get_items = spf.getData()
+                val push_items:ArrayList<Document> = Gson().fromJson(get_items, object:
+                    TypeToken<ArrayList<Document>>(){}.type)
+
                 binding.recyclerView.apply {
-                    Log.d("ImageSearchs",items.toString())
-                    adapter = SearchFragmentAdapter(items,context)
+                    Log.d("ImageSearchs","SearchFragment spf 데이터 : ${save_items}")
+                    adapter = SearchFragmentAdapter(push_items,context)
                     layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
                     setHasFixedSize(true)
                 }
 
-            } else Toast.makeText(requireContext(), "검색어를 다시 입력해주세요", Toast.LENGTH_SHORT).show()
+            } else requireContext().shortToast("잘못 입력하셨습니다.")
         }
     }
 
