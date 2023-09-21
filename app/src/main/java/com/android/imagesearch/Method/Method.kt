0304b8389f79d210d3f035c5e6f80697ba03f8f6
seongssu <loveforggong@gmail.com>
2023-09-21 23:26:50 +0900

    [FEAT] : ⚙️ datetime의 형식 변경

--- app/src/main/java/com/android/imagesearch/Method/Method.kt
+++ app/src/main/java/com/android/imagesearch/Method/Method.kt
@@ -2,7 +2,22 @@ package com.android.imagesearch.Method
 
 import android.content.Context
 import android.widget.Toast
+import java.text.SimpleDateFormat
 
 fun Context.shortToast(message: String, time: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, time).show()
+}
+
+fun timesort(time : String) : String {
+    try{
+        val input_time = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS+09:00")
+        val date = input_time.parse(time)
+
+        val output_time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        return output_time.format(date)
+    }
+    catch (e: Exception) {
+        e.printStackTrace()
+        return "Invalid input time"
+    }
 }
